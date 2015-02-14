9cce96ddfb48a3013c27a3bfdb3ec7768f057879
Julio César Aguilar Cabrera <madth3+rysi@gmail.com>
2015-02-14 11:41:43 -0600

    OAD para Articulo. Primera version.

---
+++ Clase02/ArticulosDatos/src/rysi/articulos/oad/ArticulosOAD.java
@@ -0,0 +1,49 @@
+package rysi.articulos.oad;
+
+import java.util.List;
+import rysi.articulos.entidades.Articulo;
+
+/**
+ * @author jaguilar
+ * @version 1.0
+ * @created 14-feb.-2015 11:39:53
+ */
+public interface ArticulosOAD {
+	/**
+	 * 
+	 * @param art
+	 */
+	public abstract void actualizar(Articulo art);
+
+	/**
+	 * 
+	 * @param art
+	 */
+	public abstract void borrar(Articulo art);
+
+	/**
+	 * 
+	 * @param idArticulo
+	 */
+	public abstract void borrar(int idArticulo);
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public abstract List<Articulo> buscarPorNombre(String nombre);
+
+	/**
+	 * 
+	 * @param art
+	 */
+	public abstract int crear(Articulo art);
+
+	/**
+	 * 
+	 * @param idArticulo
+	 */
+	public abstract Articulo getPorId(int idArticulo);
+
+	public abstract List<Articulo> getTodos();
+}//end ArticulosOAD
---
+++ Clase02/ArticulosDatos/src/rysi/articulos/oad/ArticulosOADJPA.java
@@ -0,0 +1,68 @@
+package rysi.articulos.oad;
+
+import java.util.List;
+import rysi.articulos.entidades.Articulo;
+
+/**
+ * @author jaguilar
+ * @version 1.0
+ * @created 14-feb.-2015 11:39:55
+ */
+public class ArticulosOADJPA implements ArticulosOAD {
+
+	public ArticulosOADJPA(){
+
+	}
+
+        /*
+	 * 
+	 * @param art
+	 */
+	public void actualizar(Articulo art){
+
+	}
+
+	/**
+	 * 
+	 * @param art
+	 */
+	public void borrar(Articulo art){
+
+	}
+
+	/**
+	 * 
+	 * @param idArticulo
+	 */
+	public void borrar(int idArticulo){
+
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public List<Articulo> buscarPorNombre(String nombre){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param art
+	 */
+	public int crear(Articulo art){
+		return 0;
+	}
+
+	/**
+	 * 
+	 * @param idArticulo
+	 */
+	public Articulo getPorId(int idArticulo){
+		return null;
+	}
+
+	public List<Articulo> getTodos(){
+		return null;
+	}
+}//end ArticulosOADJPA
