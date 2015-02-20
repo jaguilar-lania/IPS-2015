79b784a6e92e0083c02cd9933ae0e1e77f838ae1
Jesús Aguilera <jaguil8@users.noreply.github.com>
2015-02-20 13:37:30 -0600

    Tarea 02 Punto 02 Creación de otros dos objetos de Acceso a Datos(Usuarios)

---
+++ Tarea2/Jesús R. Aguilera Peña/Tarea02.2/src/rysi/proyecto/DAO/UsuariosDAO.java
@@ -0,0 +1,70 @@
+
+
+/**
+ * @author jaguilera
+ * @version 1.0
+ * @created 20-feb.-2015 13:33:20
+ */
+public class UsuariosDAO {
+
+	public UsuariosDAO(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public List<Usuario> getTodos(){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param idusuario
+	 */
+	public Usuario getPorIdUsuario(int idusuario){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public List<Usuario> buscarPorNombre(String nombre){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param idusuario
+	 */
+	public void borrar(int idusuario){
+
+	}
+
+	/**
+	 * 
+	 * @param usr
+	 */
+	public void actualizar(Usuario usr){
+
+	}
+
+	/**
+	 * 
+	 * @param usr
+	 */
+	public void borrar(Usuario usr){
+
+	}
+
+	/**
+	 * 
+	 * @param usr
+	 */
+	public void crear(Usuario usr){
+
+	}
+
+}
---
+++ Tarea2/Jesús R. Aguilera Peña/Tarea02.2/src/rysi/proyecto/DAO/UsuariosOADJPA.java
@@ -0,0 +1,70 @@
+
+
+/**
+ * @author jaguilera
+ * @version 1.0
+ * @created 20-feb.-2015 13:33:42
+ */
+public class UsuariosOADJPA extends UsuariosDAO {
+
+	public UsuariosOADJPA(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	/**
+	 * 
+	 * @param usr
+	 */
+	public void actualizar(Usuario usr){
+
+	}
+
+	/**
+	 * 
+	 * @param idusuario
+	 */
+	public void borrar(int idusuario){
+
+	}
+
+	/**
+	 * 
+	 * @param usr
+	 */
+	public void borrar(Usuario usr){
+
+	}
+
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public List<Usuario> buscarPorNombre(String nombre){
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param usr
+	 */
+	public void crear(Usuario usr){
+
+	}
+
+	/**
+	 * 
+	 * @param idusuario
+	 */
+	public Usuario getPorIdUsuario(int idusuario){
+		return null;
+	}
+
+	public List<Usuario> getTodos(){
+		return null;
+	}
+
+}
