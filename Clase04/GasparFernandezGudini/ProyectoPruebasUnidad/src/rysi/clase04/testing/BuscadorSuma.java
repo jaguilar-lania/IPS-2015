09f1e62f9441a779728adc4fd2cfef5a5d76d345
Gaspar Fdez. Gudini <nekogudini@users.noreply.github.com>
2015-02-28 14:21:44 -0600

    Envio de pruebas de unidad clase 04 - GasparFernandezGudini

---
+++ Clase04/GasparFernandezGudini/ProyectoPruebasUnidad/src/rysi/clase04/testing/BuscadorSuma.java
@@ -0,0 +1,25 @@
+package rysi.clase04.testing;
+
+import java.util.List;
+
+/**
+ *
+ * @author Gudini
+ */
+public class BuscadorSuma {
+    
+    /**
+     * Busca si existe un subconjunto cuyos elementos suman una cantidad deseada.
+     * Nota: Si la suma es cero entonces se regresara true a menos que la lista sea null.
+     * Si el conjunto es vacio entonces solo regresara true si la suma es 0.
+     * 
+     * @param conjunto Lista de enteros que representa el conjunto completo
+     * @param Suma Cantidad deseada.
+     * @return true si encuentra un subconjunto con las caracteristicas requeridas
+     * false en caso contrario
+     * @throws NullPointerException si el conjunto es null.
+     */
+    public boolean buscarSubconjuntoConSuma(List<Integer> conjunto, Integer Suma){
+        return false;
+    }
+}
