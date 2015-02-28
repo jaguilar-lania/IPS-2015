de11da85e989608ddee0f20f7c27578b8cc0cd9e
Julio Aguilar <madth3.dev@gmail.com>
2015-02-28 13:32:11 -0600

    Implementación de casos para examen 2.

--- Clase04/ProyectoPruebasUnidad/src/rysi/clase04/testing/BuscadorCadena.java
+++ Clase04/ProyectoPruebasUnidad/src/rysi/clase04/testing/BuscadorCadena.java
@@ -16,7 +16,25 @@ public class BuscadorCadena {
     public boolean buscarCadena(String aguja, String pajar) {
         return (pajar.indexOf(aguja) != -1);
     }
+    
+    
+    /**
+     * Busca una cadena dentro de otra como subsecuencia, es decir, deben aparecer 
+     * todos los caracteres de agujas en el mismo orden pero no necesariamente consecutivos.
+     * @param agujas Caracteres a buscar.
+     * @param pajar Cadena dentro de la cual se desea buscar.
+     * @return true si la cadena aguja es una subcadena de pajar.
+     * @throws NullPointerException si alguno de los parametros es null.
+     */
+    public boolean buscarCadenaComoSubsecuencia(String agujas, String pajar) {
+        return false;
+    }
 }
+
+
+
+
+
 
 
 
